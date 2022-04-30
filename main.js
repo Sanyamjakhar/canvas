23a6ff605b4b94dc61db03a008a445e09abdc106
Sanyamjakhar <71759093+Sanyamjakhar@users.noreply.github.com>
2022-04-30 21:32:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(640,480)
+canvas.position(100,250)
+}
+
+function draw(){
+
+circle(60, 60, 100);
+rect(45,60,35,400,);
+circle(200,60,100);
+rect(185,60,35,400)
+circle(340,60,100);
+rect(325,60,35,400)
+}
+
