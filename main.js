3e3bb5572373fb81576549c87e471ad900481f1f
Sanyamjakhar <71759093+Sanyamjakhar@users.noreply.github.com>
2022-05-04 20:17:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function preload(){
 function setup(){
 canvas=createCanvas(640,480)
 canvas.position(100,250)
+video=createCapture(VIDEO)
+video.hide()
 }
 
 function draw(){
@@ -15,5 +17,6 @@ circle(200,60,100);
 rect(185,60,35,400)
 circle(340,60,100);
 rect(325,60,35,400)
+image(video,400,300,200,200)
 }
 
