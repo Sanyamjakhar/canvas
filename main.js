e6823007f99d5f78ba3b0c440b888e645e993771
Sanyamjakhar <71759093+Sanyamjakhar@users.noreply.github.com>
2022-06-02 14:42:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-function preload(){
+function preload() {
 
 }
 
-function setup(){
-canvas=createCanvas(640,480)
-canvas.position(100,250)
-video=createCapture(VIDEO)
-video.hide()
+function setup() {
+    canvas = createCanvas(640, 480)
+    canvas.position(100, 250)
+    video = createCapture(VIDEO)
+    video.hide()
 }
 
-function draw(){
-
-circle(60, 60, 100);
-rect(45,60,35,400,);
-circle(200,60,100);
-rect(185,60,35,400)
-circle(340,60,100);
-rect(325,60,35,400)
-image(video,400,300,200,200)
+function draw() {
+    image(video, 200, 150, 200, 200)
+    fill("red")
+    stroke("cyan")
+    circle(60, 60, 100);
+    rect(45, 60, 35, 400, );
+    circle(585, 60, 100);
+    rect(100, 45, 500, 35)
+    circle(60, 430, 100);
+    rect(570, 60, 35, 400)
+    circle(585, 430, 100);
+    rect(100, 400, 500, 35)
 }
 
+function snap() {
+    save("san.png")
+
+}
